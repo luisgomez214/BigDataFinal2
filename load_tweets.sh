c0040f5452ab2181e126ac483c89f3918a032590
Luis <you@example.com>
2024-05-03 20:36:33 -0700

    made progress

---
+++ load_tweets.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# list all of the files that will be loaded into the database
+# for the first part of this assignment, we will only load a small test zip file with ~10000 tweets
+# but we will write are code so that we can easily load an arbitrary number of files
+files='
+test-data.zip
+'
+
+echo 'load normalized'
+for file in $files; do
+    # call the load_tweets.py file to load data into pg_normalized
+    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1321/ --inputs=$file
+done
